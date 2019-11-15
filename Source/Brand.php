961a159e14f82ef9b0a43df068839280bf95cf34
Dmitry Fedyuk <dfediuk@gmail.com>
2019-11-15 14:03:27 +0600

    no message

--- Source/Brand.php
+++ Source/Brand.php
@@ -1,35 +1,26 @@
 <?php
 namespace Justuno\M2\Source;
 // 2019-11-15
-/** @final Unable to use the PHP «final» keyword here because of the M2 code generation. */
-class Brand implements \Magento\Framework\Option\ArrayInterface {
+final class Brand extends \Df\Config\Source {
 	/**
-	 * {@inheritdoc}
-	 *
-	 * @codeCoverageIgnore
-	 */
-	function toOptionArray()
-	{
-
-		$objectManager = \Magento\Framework\App\ObjectManager::getInstance();
-		$resource   = $objectManager->get('Magento\Framework\App\ResourceConnection');
-		$connection = $resource->getConnection();
-		$tableName  = $resource->getTableName('eav_attribute');
-		$Query      = "Select * FROM " . $tableName. " where entity_type_id=4";
-		$result     = $connection->fetchAll($Query);
-		$attributes = array();
-		$attributes[] = ['value' => '', 'label' => __('Please Select')];
-
-		foreach ($result as $a) {
-			/**
-			 * 2019-10-25 Dmitry Fedyuk https://www.upwork.com/fl/mage2pro
-			 * "The `manufaturer` attribute is absent in the «Choose Brand Attribute» backend dropdown":
-			 * https://github.com/justuno-com/m2/issues/3
-			 */
-			if (!$a['is_user_defined'] || 'manufacturer' === $a['attribute_code']) {
-				$attributes[] = ['value' => $a['attribute_code'], 'label' => __($a['frontend_label'])];
-			}
-		}
-		return $attributes;
-	}
+	 * 2019-11-15
+	 * @override
+	 * @see \Df\Config\Source::map()
+	 * @used-by \Df\Config\Source::toOptionArray()
+	 * @return array(string => string)
+	 */	
+	protected function map() {return df_map_0(array_column(
+		array_filter(
+			df_fetch('eav_attribute', ['attribute_code', 'frontend_label', 'is_user_defined'], 'entity_type_id', 4)
+			,function(array $a) {return
+				/**
+				 * 2019-10-25 Dmitry Fedyuk https://www.upwork.com/fl/mage2pro
+				 * "The `manufaturer` attribute is absent in the «Choose Brand Attribute» backend dropdown":
+				 * https://github.com/justuno-com/m2/issues/3
+				 */
+				!$a['is_user_defined'] || 'manufacturer' === $a['attribute_code']
+			;}
+		)
+		,'frontend_label', 'attribute_code'
+	));}
 }
