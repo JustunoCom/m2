5ccd36b6db6a10eb0f6503ade491c60140050d0f
Dmitry Fedyuk <dfediuk@gmail.com>
2020-05-06 20:47:58 +0600

    https://github.com/justuno-com/m2/issues/13

--- Controller/Response/Inventory.php
+++ Controller/Response/Inventory.php
@@ -24,23 +24,14 @@ class Inventory extends _P {
 	 */
 	function execute() {return R::p(function() {
 		$pc = df_product_c(); /** @var PC $pc */
-		$pc->addAttributeToSelect('*');
 		/**
-		 * 2019-10-30
+		 * 2020-05-06
 		 * 1) «We don't want to include products that have been disabled or have only disabled variants»:
 		 * https://github.com/justuno-com/m2/issues/13#issue-612869130
 		 * 2) @uses \Magento\Catalog\Model\ResourceModel\Product\Collection::setVisibility()
 		 * filters out the disabled products.
 		 */
 		$pc->setVisibility([V::VISIBILITY_BOTH, V::VISIBILITY_IN_CATALOG, V::VISIBILITY_IN_SEARCH]);
-		/**
-		 * 2019-11-22
-		 * @uses \Magento\Catalog\Model\ResourceModel\Product\Collection::addMediaGalleryData() loads the collection,
-		 * so we should apply filters before it, not after.
-		 * «Filters do not work for `catalog`»: https://github.com/justuno-com/m2/issues/5
-		 */
-		return array_values(array_map(function(P $p) {return [
-			'ID' => $p->getId(), 'Variants' => cVariants::p($p)
-		];}, $pc->getItems()));
+		return array_values(df_map($pc, function(P $p) {return ['ID' => $p->getId(), 'Variants' => cVariants::p($p)];}));
 	}, true);}
 }
