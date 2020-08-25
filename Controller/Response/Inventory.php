f1a2f533d67f39ae09dfb7ab2339583576838468
Dmitry Fedyuk <dfediuk@gmail.com>
2020-08-25 16:52:47 +0600

    https://github.com/justuno-com/core/issues/60

--- Controller/Response/Inventory.php
+++ Controller/Response/Inventory.php
@@ -21,7 +21,7 @@ class Inventory extends _P {
 	 * https://github.com/magento/magento2/blob/2.2.1/lib/internal/Magento/Framework/App/Action/Action.php#L84-L125
 	 * @return Json
 	 */
-	function execute() {return R::p(function() {return array_values(df_map(
+	function execute() {return R::p(function() {return array_values(ju_map(
 		function(P $p) {return ['ID' => $p->getId(), 'Variants' => cVariants::p($p)];}
 		/**
 		 * 2020-05-06
