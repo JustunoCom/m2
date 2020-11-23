6aa80a29f365d2dde6a3cdf2a1a2be3422be5521
Dmitry Fedyuk <dfediuk@gmail.com>
2020-11-24 05:34:44 +0600

    1.4.7: https://github.com/justuno-com/m2/issues/23

--- Controller/Response/Catalog.php
+++ Controller/Response/Catalog.php
@@ -26,6 +26,7 @@ class Catalog extends _P {
 	 * @return Json
 	 */
 	function execute() {return R::p(function() {
+		ju_pc_disable_flat();
 		$pc = ju_pc(); /** @var PC $pc */
 		$pc->addAttributeToSelect('*');
 		/**
@@ -41,7 +42,7 @@ class Catalog extends _P {
 			V::VISIBILITY_BOTH, V::VISIBILITY_IN_CATALOG, V::VISIBILITY_IN_SEARCH
 		]]);
 		# 2020-11-23
-		# @todo If the flat mode is enabled, then at this point the collection already misses diabled products,
+		# @todo If the flat mode is enabled, then at this point the collection already misses disabled products,
 		# because the `catalog_product_flat_<store>` table does not contain disabled products at least in Magento 2.4.0.
 		# It is wrong because disabled products should be in the feed: https://github.com/justuno-com/m2/issues/19
 		/**
