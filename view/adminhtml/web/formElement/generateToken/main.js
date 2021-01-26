e9810f3a20b5a0aaf5d904a9e44eb9606cdd3610
Dmitry Fedyuk <dfediuk@gmail.com>
2021-01-27 03:20:07 +0600

    no message

--- view/adminhtml/web/formElement/generateToken/main.js
+++ view/adminhtml/web/formElement/generateToken/main.js
@@ -7,16 +7,15 @@ define(['jquery', 'domReady!'], function($) {return (
 	function(config) {
 		/** @type {jQuery} HTMLButtonElement */ var $e = $(document.getElementById(config.id));
 		$e.click(function() {
-			function randomString(chars) {
+			document.getElementById('justuno_settings_options_interface_token_key').value = function() {
 				var r = '';
-				for (var i = 32; i > 0; --i) {
-					r += chars[Math.floor(Math.random() * chars.length)];
+				const c = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+				const cl = c.length;
+				for (let i = 32; 0 < i; i--) {
+					r += c[Math.floor(cl * Math.random())];
 				}
 				return r;
-			}
-			document.getElementById('justuno_settings_options_interface_token_key').value = randomString(
-				'0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-			);
+			}();
 		});
 	}
 );});
