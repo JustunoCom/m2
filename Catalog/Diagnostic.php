5d907def1c5f1d8d875b36e67bbc9285452c9e2d
Dmitry Fedyuk <dfediuk@gmail.com>
2021-02-25 04:49:17 +0600

    ttps://github.com/justuno-com/m2/issues/32

---
+++ Catalog/Diagnostic.php
@@ -0,0 +1,14 @@
+<?php
+namespace Justuno\M2\Catalog;
+use Justuno\Core\Exception as DFE;
+# 2021-02-25
+# "Provide a diagnostic message if the requested product is not eligible for the feed":
+# https://github.com/justuno-com/m2/issues/32
+final class Diagnostic {
+	/**
+	 * 2021-02-25
+	 * @used-by \Justuno\M2\Controller\Response\Catalog::execute()
+	 * @throws DFE
+	 */
+	static function p() {ju_error('Diagnostic...');}
+}
