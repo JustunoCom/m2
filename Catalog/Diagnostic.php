16e8eb01ef51cd7f4f113ae95cc78794878e9ca6
Dmitry Fedyuk <dfediuk@gmail.com>
2021-02-25 07:34:56 +0600

    1.5.9: https://github.com/justuno-com/m2/issues/32

--- Catalog/Diagnostic.php
+++ Catalog/Diagnostic.php
@@ -13,17 +13,21 @@ final class Diagnostic {
 	 */
 	static function p() {
 		$id = (int)ju_request('id'); /** @var int $id */
-		if (!ju_fetch_one('catalog_product_entity', 'entity_id', ['entity_id' => $id])) {
+		if (!($u = ju_fetch_one('catalog_product_entity', 'updated_at', ['entity_id' => $id]))) { /** @var string $u */
 			ju_error("The product with id «{$id}» is absent in the `catalog_product_entity` table.");
 		}
 		$ww = ju_fetch_col_int('catalog_product_website', 'website_id', 'product_id', $id); /** @var int[] $ww */
+		$prefix = "The product with id «{$id}» is not eligible for the feed "; /** @var string $prefix */
 		if (!in_array($w = S::v()->getWebsiteId(), $ww)) { /** @var int $w */
 			ju_error(
-				"The product with id «{$id}» is not eligible for the feed because it is associated with websites [%s],"
+				"{$prefix}because it is associated with websites [%s],"
 				. " but the feed is generated for the website «{$w}» (according to the token provided)."
 				,ju_csv_pretty(ju_quote_russian($ww))
 			);
 		}
-		ju_error('The product is not eligible for the feed for an unknown reason.');
+		if (($updatedSince = ju_request('updatedSince')) && ju_date_lt(ju_date_from_db($u), ju_date_from_db($updatedSince))) {
+			ju_error("{$prefix}because it was updated at {$u}.");
+		}
+		ju_error("{$prefix}for an unknown reason.");
 	}
 }
